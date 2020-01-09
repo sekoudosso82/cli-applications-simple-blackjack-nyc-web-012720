c63b6a658bbd083dec562ef36d14cbbc48e6f7be
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-09 03:15:21 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,68 @@
+require "pry"
 def welcome
-  # code #welcome here
+  puts ("Welcome to the Blackjack Table")
 end
 
 def deal_card
-  # code #deal_card here
+  randon_number = rand(1..11) 
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(integer_args)
+ puts "Your cards add up to #{integer_args}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+    result = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game (arg)
+  puts "Sorry, you hit #{arg}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  a = deal_card 
+  b = deal_card 
+  r = a+b 
+  display_card_total (r)
+  r 
 end
 
-def hit?
-  # code hit? here
-end
 
 def invalid_command
-  # code invalid_command here
+   puts("Please enter a valid command")
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+def hit?( arg)
+  prompt_user
+  input_ = get_user_input
+  player_current_total = arg
+  if input_ == "s" # we don't deal a new card
+    return player_current_total 
+  end
+  # binding.pry
+  # player_current_total = initial_round
+  if input_ == "h" # we do need to deal a new
+    new_card = deal_card.to_i
+    player_current_total = player_current_total+new_card
+    return player_current_total
+  end 
+  
+  if !(input_ == ("s" && "h"))
+    invalid_command
+    hit?(arg)
+  end 
+  # binding.pry
+end
 
 def runner
-  # code runner here
+welcome
+first = initial_round
+second = hit?( first)
+display_card_total(second)  
+end_game (second)
 end
     
